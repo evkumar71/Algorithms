707fa0a5d54cda3e4c6eb4a868a039ff969d3e1e
Vijaya K Eyunni <evkumar71@yahoo.com>
2023-03-26 10:22:02 -0700

    tested palindrom partn

--- palpart.py
+++ palpart.py
@@ -1,18 +1,22 @@
 def palpart(s):
 
-    def back(s, start):
+    def back(s, start, path=[]):
         #print(f"Len: {len(s[start:])} : {s[start:]}")
         if start >= len(s):
+            res.append(path)
             return
 
         for end in range(start, len(s)):
             temp = s[start:end+1]
             # print(f"{start}: {temp}")
             if temp == temp[::-1]:
-                print(f"Pal: {temp} from {start} for {s[start:]}")
-            back(s, end+1)
+                #print(f"Pal: {temp} from {start} for {s[start:]}")
+                back(s, end+1, path + [temp])
 
+    res = []
     back(s, 0)
+    print(f"Res: {res}")
 
-s = 'aab'
+s = 'aabab'
+print(f"orig: {s}")
 palpart(s)
